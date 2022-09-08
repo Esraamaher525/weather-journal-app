cda1652e1dbbcb3f66b829a027e213a277652cb3
Esraa Maher AboDahab Ghaleb <Esraa.Maher@andalusia.loc>
2022-09-08 18:14:24 +0200

    appear data in html from git request

--- server.js
+++ server.js
@@ -1,38 +1,39 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-const PORT=4000;
-const hostname="127.0.0.1";
+const PORT = 4000;
+const hostname = "127.0.0.1";
 // Require Express to run server and routes
-const express=require("express");
+const express = require("express");
 // Start up an instance of app
-const app=express();
-app.use(express.json()); 
+const app = express();
+app.use(express.json());
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-const bodyParser= require("body-parser");
+const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors=require("cors");
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Setup Server
- app.listen(PORT,hostname,()=>{
-    console.log(`port is ${PORT}`);
- })
- //get request
- app.get('/all',(req,res)=>{
-    res.send(projectData)
- })
- //post request
- app.post('/sendWeatherData',(req,res)=>{
-    projectData={...req.body};
-    res.end();
- })
+app.listen(PORT, hostname, () => {
+   console.log(`port is ${PORT}`);
+})
+//post request
+app.post('/sendWeatherData', (req, res) => {
+   projectData = { ...req.body };
+   res.end();
+})
+//get request
+app.get('/all', (req, res) => {
+   res.send(projectData)
+})
+
 
 
 
--- website/app.js
+++ website/app.js
@@ -1,42 +1,46 @@
 /* Global Variables */
-const apiKey="4ef2d3191bcfeacc2f56140f8cf05f57";
-const zipCodeUrl="https://api.openweathermap.org/data/2.5/weather";
-const feelingElement=document.getElementById("feelings");
+const apiKey = "4ef2d3191bcfeacc2f56140f8cf05f57";
+const zipCodeUrl = "https://api.openweathermap.org/data/2.5/weather";
+const feelingElement = document.getElementById("feelings");
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = (d.getMonth() + 1) + '.' + d.getDate() + '.' + d.getFullYear();
 
 //event when click generate
-document.getElementById("generate").addEventListener("click",async ()=>{
+document.getElementById("generate").addEventListener("click", async () => {
     //get zip code data
-    let zipCodeData=document.querySelector("#zip").value;
-    if(zipCodeData){
-    //fetch data with then , catch
+    let zipCodeData = document.querySelector("#zip").value;
+    if (zipCodeData) {
+        //fetch data with then , catch
         // let weatherAPIData={};
         // fetch(`${zipCodeUrl}?zip=${zipCodeData}&appid=${apiKey}`)
         // .then(res=>res.json())
         // .then(data=>weatherAPIData=data)
         // .catch(err=>console.log(err))
-    //fetch data with async,await
-        const res =await fetch(`${zipCodeUrl}?zip=${zipCodeData}&appid=${apiKey}`)
-        const weatherAPIData=await res.json();
-    //post data to server
-        fetch('/sendWeatherData',{
-            method:"POST",
-            headers:{
-                'Content-Type':'application/json'
+        //fetch data with async,await
+        const res = await fetch(`${zipCodeUrl}?zip=${zipCodeData}&appid=${apiKey}`)
+        const weatherAPIData = await res.json();
+        //post data to server
+        fetch('/sendWeatherData', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify({
-                data:newDate,
-                content:feelingElement.value,
-                temp:weatherAPIData.main.temp
+            body: JSON.stringify({
+                date: newDate,
+                content: feelingElement.value,
+                temp: weatherAPIData.main.temp
             })
         })
-       
-    //get all data from server
-        const streamData =await fetch('/all')
-        const allData=await streamData.json();
+
+        //get all data from server
+        const streamData = await fetch('/all')
+        const allData = await streamData.json();
         console.log(allData)
+        //set values to elements
+        document.getElementById("date").innerHTML=`Date is : ${allData.date}`;
+        document.getElementById("temp").innerHTML=`Temp is : ${Math.round(allData.temp)} degrees`;
+        document.getElementById("content").innerHTML=`Content is : ${allData.content}`;
     }
-  
+
 })
